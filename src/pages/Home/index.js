06e6fc1806cd91f9079f8a4e6a534150d2582c22
Micael Lopes <micaelopes32@gmail.com>
2020-05-08 00:21:23 -0300

    Writing components structure for Home produto list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
+import { FiShoppingBag } from 'react-icons/fi';
+
 // import { Container } from './styles';
 
 export default class Home extends Component {
   render() {
-    return <h1>Início</h1>;
+    return (
+      <ProductList>
+        <li>
+          <img
+            src="https://cdn.awsli.com.br/800x800/1060/1060828/produto/42168015/1ae3f14322.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis Mizuno Dourado Luxo</strong>
+          <span>R$599,90</span>
+
+          <button type="button">
+            <div>
+              <FiShoppingBag size={36} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR NA SACOLA</span>
+          </button>
+        </li>
+      </ProductList>
+    );
   }
 }
