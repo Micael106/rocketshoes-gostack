b7d093828f6d60fcd42727f642a517b19715fa06
Micael Lopes <micaelopes32@gmail.com>
2020-05-07 23:51:43 -0300

    Added Cart Component and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiShoppingBag } from 'react-icons/fi';
 
-import { Container } from './styles';
+import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
@@ -8,6 +9,14 @@ export default function Header() {
   return (
     <Container>
       <img src={logo} alt="Rocketshoes" />
+
+      <Cart to="/cart">
+        <div>
+          <strong>Sacola de Compras</strong>
+          <span>3 itens</span>
+        </div>
+        <FiShoppingBag size={36} color="#FFF" />
+      </Cart>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,8 +1,33 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 50px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  text-decoration: none;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #aaa;
+      margin-top: 5px;
+    }
+  }
 `;
