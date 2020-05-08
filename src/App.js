9c7fb13e4b4314f41f13fd3ef880cf719fa61796
Micael Lopes <micaelopes32@gmail.com>
2020-05-07 23:54:18 -0300

    Fix Header component position on App.js render method

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyle />
-      <Header />
     </BrowserRouter>
   );
 }
