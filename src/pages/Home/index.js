f1ea3ebd5dc3ef33feb9f3766a7c4a8139c09b65
Micael Lopes <micaelopes32@gmail.com>
2020-05-16 23:36:27 -0300

    Added fake api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,58 +4,39 @@ import { FiShoppingBag } from 'react-icons/fi';
 
 import { ProductList } from './styles';
 
+import api from '../../services/api';
+
 export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('http://localhost:3333/products');
+
+    this.setState({ products: response.data });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://cdn.awsli.com.br/800x800/1060/1060828/produto/42168015/1ae3f14322.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis Mizuno Dourado Luxo</strong>
-          <span>R$599,90</span>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
 
-          <button type="button">
-            <div>
-              <FiShoppingBag size={24} color="#FFF" />
-            </div>
+            <button type="button">
+              <div>
+                <FiShoppingBag size={24} color="#FFF" />
+              </div>
 
-            <span>ADICIONAR NA SACOLA</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://cdn.awsli.com.br/800x800/1060/1060828/produto/42168015/1ae3f14322.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis Mizuno Dourado Luxo</strong>
-          <span>R$599,90</span>
-
-          <button type="button">
-            <div>
-              <FiShoppingBag size={24} color="#FFF" />
-            </div>
-
-            <span>ADICIONAR NA SACOLA</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://cdn.awsli.com.br/800x800/1060/1060828/produto/42168015/1ae3f14322.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis Mizuno Dourado Luxo</strong>
-          <span>R$599,90</span>
-
-          <button type="button">
-            <div>
-              <FiShoppingBag size={24} color="#FFF" />
-            </div>
-
-            <span>ADICIONAR NA SACOLA</span>
-          </button>
-        </li>
+              <span>ADICIONAR NA SACOLA</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
