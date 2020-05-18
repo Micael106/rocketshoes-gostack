e0d5b0dd3800177746e3bae20c52703881f32fec
Micael Lopes <micaelopes32@gmail.com>
2020-05-18 00:17:57 -0300

    Installed and Configured Reactotron for debug redux in development enviroment

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { FiShoppingBag } from 'react-icons/fi';
 
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -16,10 +17,16 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Sacola de Compras</strong>
-          <span>3 itens</span>
+          <span>
+            {cartSize} {cartSize > 1 ? 'itens' : 'item'}
+          </span>
         </div>
         <FiShoppingBag size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
+
+export default connect((state) => ({
+  cartSize: state.cart.length,
+}))(Header);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,4 +1,4 @@
-export default function cart(state, action) {
+export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
       return [...state, action.product];
